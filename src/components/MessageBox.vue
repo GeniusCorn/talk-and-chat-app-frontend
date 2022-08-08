0436c7b961a6d60d9fe2f255788d2c08773ae3ea
GeniusCorn <hi@nicecorn.com>
2022-08-08 23:12:39 +0800

    :sparkles: 新增消息盒子

---
+++ src/components/MessageBox.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex p-3 border">
+    <var-icon name="account-circle-outline" :size="50" />
+    <div class="ml-2">
+      <div class="">用户名</div>
+      <div>消息</div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
