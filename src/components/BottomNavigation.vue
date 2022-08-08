4059dc04f0936d76e7c1f2d9a818cfed1cffc0be
GeniusCorn <hi@nicecorn.com>
2022-08-08 22:42:31 +0800

    :sparkles: 新增底部导航栏

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -5,7 +5,7 @@ const active = ref(0);
 </script>
 
 <template>
-  <var-bottom-navigation v-model:active="active">
+  <var-bottom-navigation v-model:active="active" class="bottom-nav">
     <var-bottom-navigation-item
       label="聊天"
       icon="message-processing-outline"
@@ -17,3 +17,10 @@ const active = ref(0);
     <var-bottom-navigation-item label="我的" icon="home" />
   </var-bottom-navigation>
 </template>
+
+<style scoped>
+.bottom-nav {
+  position: fixed;
+  bottom: 0;
+}
+</style>
