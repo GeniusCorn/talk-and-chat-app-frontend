357ca8d010213639a8e8819fadf8327c3e9a7f26
GeniusCorn <hi@nicecorn.com>
2022-08-13 09:58:27 +0800

    :wrench: 添加代码约束忽略规则

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true,
+    }),
     components({
       resolvers: [VarletUIResolver()],
     }),
