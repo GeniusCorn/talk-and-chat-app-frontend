141b10188cd891aac2a2b0681615fc39d62d2f2f
GeniusCorn <hi@nicecorn.com>
2022-08-14 17:56:21 +0800

    :sparkles: 登录页面

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import LoginForm from '@/components/login/LoginForm.vue';
+</script>
+
+<template>
+  <div class="h-screen grid grid-rows-3 gap-4">
+    <div class="h-max place-self-center">
+      <span class="text-3xl">欢迎使用聊天软件</span>
+    </div>
+    <LoginForm></LoginForm>
+    <div class="fixed bottom-0 right-0">
+      <span class="text-xs"> Powered by @GeniusCorn </span>
+    </div>
+  </div>
+</template>
