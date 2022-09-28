cae1e09909d52ce1a2550368eb2ada93b527ca9d
GeniusCorn <hi@nicecorn.com>
2022-09-28 22:05:05 +0800

    style: top header

---
+++ src/components/common/TopHeader.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+onMounted(() => {
+  const { path } = route
+
+  switchRoute(path)
+})
+
+watch(route, () => {
+  const { path } = route
+
+  switchRoute(path)
+})
+
+let title: string = $ref('')
+
+function switchRoute(path: string) {
+  switch (path) {
+    case '/message':
+      title = '消息'
+      break
+
+    case '/address':
+      title = '好友'
+      break
+
+    case '/settings':
+      title = '设置'
+      break
+
+    default:
+      break
+  }
+}
+</script>
+
+<template>
+  <div
+    bg-pink-400
+    h-14
+    flex="~"
+    justify-center
+    items-center
+    text-white
+    font-semibold
+  >
+    {{ title }}
+  </div>
+</template>
+
+<style scoped></style>
