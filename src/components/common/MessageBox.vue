f4c63d1c60b3834dbf9dec80670ffa7839482861
GeniusCorn <hi@nicecorn.com>
2022-09-28 22:57:05 +0800

    style: message box

---
+++ src/components/common/MessageBox.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div py-2 px-4 flex="~ row" border-b border-gray-200 items-center>
+    <div i-tabler-user-circle text-5xl />
+
+    <div ml-2>
+      <div font-bold>联系人</div>
+      <div text-xs truncate class="w-[70vw]">
+        10000000000000000000000000000000000000000000000
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
