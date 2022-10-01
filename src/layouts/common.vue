2c2e3ac4aee0df71314c3594656cd28c6c3fd227
GeniusCorn <hi@nicecorn.com>
2022-10-01 12:52:16 +0800

    style: router transition

--- src/layouts/common.vue
+++ src/layouts/common.vue
@@ -2,8 +2,25 @@
 
 <template>
   <TopHeader />
-  <RouterView />
+
+  <RouterView v-slot="{ Component, route }">
+    <Transition mode="out-in">
+      <div :key="route.path">
+        <component :is="Component" />
+      </div>
+    </Transition>
+  </RouterView>
+
   <MenuBar />
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
